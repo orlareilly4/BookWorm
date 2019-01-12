a8fdef774a4466546eb800f7a1f944fdaf1fbd7d
orlareilly <orlareilly4@gmail.com>
2019-01-12 18:55:09 +0000

    Iteration Three - Add to Library

--- app/src/main/java/info/androidhive/firebase/LibraryActivity.java
+++ app/src/main/java/info/androidhive/firebase/LibraryActivity.java
@@ -2,15 +2,108 @@ package info.androidhive.firebase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LibraryActivity extends AppCompatActivity {
+
+
+    EditText editTextName;
+    Button buttonAdd;
+    Spinner spinnerGenres;
+
+    DatabaseReference databaseArtists;
+
+    ListView listViewArtists;
+    List <Artist> artistList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library);
 
+        databaseArtists = FirebaseDatabase.getInstance().getReference("artists");
 
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        buttonAdd = (Button) findViewById(R.id.buttonAddArtist);
+        spinnerGenres = (Spinner) findViewById(R.id.spinnerGenres);
+
+        listViewArtists = (ListView) findViewById(R.id.listViewArtists);
+        artistList = new ArrayList<>();
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                addArtist();
+            }
+        });
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        databaseArtists.addValueEventListener(new ValueEventListener() {
+            @Override //fetches value that are stored in the database
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                artistList.clear();
+                for (DataSnapshot artistSnapshot : dataSnapshot.getChildren()) {
+                    Artist artist = artistSnapshot.getValue(Artist.class);
+
+                    artistList.add(artist);
+                }
+
+
+                ArtistList adapter = new ArtistList(LibraryActivity.this, artistList);
+                listViewArtists.setAdapter(adapter);
+
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void addArtist() {
+        String name = editTextName.getText().toString().trim();
+        String genre = spinnerGenres.getSelectedItem().toString();
+
+        if(!TextUtils.isEmpty(name)){
+            String id = databaseArtists.push().getKey();
+
+            Artist artist = new Artist(id,name,genre);
+
+            databaseArtists.child(id).setValue(artist);
+            Toast.makeText(this, "Artist added", Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(this,"Please enter a name", Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
